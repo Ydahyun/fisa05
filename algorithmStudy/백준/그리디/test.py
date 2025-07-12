ef2e189cedbee77551947bea7c53829ea4255219
Ydahyun~ <y7d3h0@naver.com>
2025-07-12 16:13:54 +0900

    Add 29767.py

--- algorithmStudy/백준/그리디/test.py
+++ algorithmStudy/백준/그리디/test.py
@@ -1,12 +1,16 @@
-import sys
-input = sys.stdin.readline
 
-while True:
-    try:
-        # 입력
-        A, B, C = map(int, input().split())
+n, k = 5, 3
+list_score = [3,-5,2,-1,4]
 
-        # 간격이 넓은 곳으로 착지
-        print(max(B-A, C-B)-1)
-    except:
-        break
+# 교실 초기 값 세팅
+cnt = 0  # 교실 방문횟수
+class_info = []  # 교실의 가중치(int)와 방문횟수(int), 학생들의 목적지(bool)로 2차원 리스트로 넣어줄거임
+for score in list_score:
+    class_info.append([score, cnt, False])
+
+                     # [[교실가중치, 방문횟수, 학생들의 목적지]]
+# print(class_info)  # [[3, 0, False], [-5, 0, False], [2, 0, False], [-1, 0, False], [4, 0, False]]
+
+class_info = [[3, 0, False], [-5, 0, False], [2, 0, False], [-1, 0, False], [4, 0, False]]
+for i, val in enumerate(class_info):
+    print(val[0], class_info[i+1][0])

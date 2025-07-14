35359c1208e7a88b41a30f03a7d3fe9611083fae
Ydahyun~ <y7d3h0@naver.com>
2025-07-15 00:51:45 +0900

    Add 단계별학습

---
+++ algorithmStudy/백준/단계별학습/9086.py
@@ -0,0 +1,36 @@
+# 백준 9086번
+# <문제> 문자열: 문제 설명
+"""
+
+"""
+
+# 입력
+"""
+
+"""
+
+# 출력
+"""
+
+"""
+
+# 내 풀이 아이디어
+"""
+
+"""
+
+n = int(input())
+list_s=[]
+for i in range(n):
+    list_s.append(input())
+
+for i in range(n):
+    print(list_s[i][0]+list_s[i][-1])
+
+
+# 동빈나 풀이
+
+# Discussion
+"""
+
+"""

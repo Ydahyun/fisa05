de37b97257f1b172672e8d3521e8ee6ae9b010df
Ydahyun~ <y7d3h0@naver.com>
2025-07-17 23:45:33 +0900

    Add 2675.py

---
+++ algorithmStudy/백준/단계별학습/2675.py
@@ -0,0 +1,32 @@
+# 백준 2675번
+# <문제> 문자열 반복: 문제 설명
+"""
+
+"""
+
+# 입력
+"""
+
+"""
+
+# 출력
+"""
+
+"""
+
+# 내 풀이 아이디어
+"""
+
+"""
+
+n=int(input())
+for _ in range(n):
+    s=input().split()
+    for i in range(len(s[1])):
+        print(s[1][i]*int(s[0]), end="")
+    print()
+
+# Discussion
+"""
+
+"""

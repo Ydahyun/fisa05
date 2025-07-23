f5cce74525a2625b05dd53adade53834c62e01bc
Ydahyun <y7d3h0@naver.com>
2025-07-23 14:30:44 +0900

    update

--- algorithmStudy/백준/단계별학습/브루트포스/2231.py
+++ algorithmStudy/백준/단계별학습/브루트포스/2231.py
@@ -1,7 +1,16 @@
 # 백준 2231번
 # <문제> 분해합: 문제 설명
 """
+245+2+4+5
+245+11=256
 
+198+1+9+8
+198+18=216
+
+216
+
+1+2+3+123
+6+123=129
 """
 
 # 입력
@@ -18,7 +27,13 @@
 """
 
 """
+from itertools import combinations
 
+num_list = list(map(int, input().split()))  # int형으로 담으려면 list()해줘야함.
+
+
+
+combi = list(combinations(num_list, 3))
 
 # Discussion
 """

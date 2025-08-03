88900cfe29fbefa8cec0e5649712fc46782e3a65
Ydahyun~ <y7d3h0@naver.com>
2025-08-04 00:48:47 +0900

    update

---
+++ algorithmStudy/백준/단계별학습/스택큐덱1/큐/18258_큐2.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+
+input=sys.stdin.readline
+queue = deque()
+
+# queue.append(5)
+# queue.popleft()
+
+
+N = int(input())
+
+for _ in range(N):
+    order = input().split()
+    
+    if order[0]=='push':
+        queue.append(order[1])
+    elif order[0]=='pop':
+        if len(queue) ==0:
+            print(-1)
+        else: print(int(queue.popleft()))
+    elif order[0]=='size':
+        print(len(queue))
+    elif order[0]=='empty':
+        if len(queue)==0:
+            print(1)
+        else: print(0)
+    elif order[0]=='front':
+        if len(queue)==0:
+            print(-1)
+        else: print(int(queue[0]))
+    elif order[0]=='back':
+        if len(queue)==0:
+            print(-1)
+        else: print(int(queue[-1]))

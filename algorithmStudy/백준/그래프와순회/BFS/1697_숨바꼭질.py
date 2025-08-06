b6d885fea21a3babac7f9648362dcedf5e158da8
Ydahyun <y7d3h0@naver.com>
2025-08-06 19:01:08 +0900

    update

--- algorithmStudy/백준/그래프와순회/BFS/1697_숨바꼭질.py
+++ algorithmStudy/백준/그래프와순회/BFS/1697_숨바꼭질.py
@@ -5,12 +5,11 @@ N, K = map(int, input().split())
 graph = [0 for _ in range(0, 100_000+1)]
 # print(len(graph))# 100_001
 
-cnt=0
 
-def bfs(graph, x, y):
+visited = [0]*(100_000+1)
+def bfs(graph, x, y, visited):
     queue = deque()
     queue.append(x)
-    global cnt
 
     while queue:
 
@@ -20,7 +19,7 @@ def bfs(graph, x, y):
         for i in range(3):
             nx = x + dx[i]
             # cnt +=1
-
+            
             # 이탈하면 컨티뉴
             if nx <0 or nx >1e5:
                 continue
@@ -28,8 +27,9 @@ def bfs(graph, x, y):
             else:
                 queue.append(nx)
                 graph[nx] = graph[x] + 1
+                visited[nx] += graph[nx] +1
+                #print(graph[nx])
             if nx == y:
-                break
-    return graph[nx]
+                return visited[nx]
                 
-print(bfs(graph, N-1, K-1))
+print(bfs(graph, N-1, K-1, visited)+1)

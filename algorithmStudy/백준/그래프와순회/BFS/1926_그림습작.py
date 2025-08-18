7fd62777db172ae4809650d3f4664569278f5760
Ydahyun <y7d3h0@naver.com>
2025-08-18 11:49:09 +0900

    add 그림습작

--- algorithmStudy/백준/그래프와순회/BFS/1926_그림습작.py
+++ algorithmStudy/백준/그래프와순회/BFS/1926_그림습작.py
@@ -1,50 +1,39 @@
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split()) # n 행, m 열
-
-graph = []
+n,m=map(int, input().split())
+graph=[]
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-# for i in graph:
-#     print(i)
+# print(graph)
 
-# [1, 1, 0, 1, 1]
-# [0, 1, 1, 0, 0]
-# [0, 0, 0, 0, 0]
-# [1, 0, 1, 1, 1]
-# [0, 0, 1, 1, 1]
-# [0, 0, 1, 1, 1]
-
+visited = [[0]*m for _ in range(n)]
 dx=[1,-1,0,0]
 dy=[0,0,1,-1]
-visited = [[0]*m for _ in range(n)]
 
 def bfs(x,y,visited):
-    
     queue = deque()
     queue.append((x,y))
     cnt=0
+
     while queue:
         x,y = queue.popleft()
         visited[x][y] = 1
-        
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
             if nx < 0 or nx >= n or ny <0 or ny >= m:
                 continue
-           
 
             if graph[nx][ny] == 1 and visited[nx][ny] == 0:
                 visited[nx][ny] = 1
                 queue.append((nx,ny))
-        cnt +=1
-              
+        cnt+=1
     return cnt
 
 
